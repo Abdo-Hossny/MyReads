4dce5d1ea382c66f22a47f22281ad0af16ed54fd
Abdo-Hossny <abdo_hossny89@yahoo.com>
2022-10-25 10:20:06 +0200

    Add NotFound.js

---
+++ starter/src/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Components/Header";
+
+const NotFound = () => {
+    return(
+        <>
+        <Header />
+        <h1>Sorry, Page not Found</h1>
+        </>
+    )
+    
+}
+
+export default NotFound
